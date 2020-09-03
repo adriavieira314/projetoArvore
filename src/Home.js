3d6499842e45bde31b0e8c9417b1dcc2a9c93285
Adria Vieira <adriavieira314@gmail.com>
2020-09-03 16:40:46 -0400

    trilhas adicionadas

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,15 @@ function App() {
           <li>Entrar</li>
         </ul>
       </header>
+
+      <section id='container'>
+        <p className='grid-item'>Trilha Botânica</p>
+        <p className='grid-item'>Trilha Monteiro Lobato</p>
+        <p className='grid-item'>Trilha Universo</p>
+        <p className='grid-item'>Trilha Folclore</p>
+        <p className='grid-item'>Trilha Matemática</p>
+        <p className='grid-item'>Trilha Experimentos</p>
+      </section>
       
     </main>
   );
