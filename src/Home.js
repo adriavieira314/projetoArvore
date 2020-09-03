1b40d4e0f39726c6a2ba81f7907118389d227afd
Adria Vieira <adriavieira314@gmail.com>
2020-09-03 16:04:13 -0400

    header adicionado

--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './Home.css';
 
 function App() {
   return (
     <div>
+      <header>
+        <ul>
+          <li>Sobre nós</li>
+          <li>Nossas Trilhas</li>
+          <li>Entrar</li>
+        </ul>
+      </header>
       
     </div>
   );
