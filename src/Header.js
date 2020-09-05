b8b5f67ab93842e59e2d0b5c3363c6ca97eefb40
Adria Vieira <adriavieira314@gmail.com>
2020-09-05 13:24:45 -0400

    usuário adicionado

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Home.css';
+import user from './assets/user.jpg';
 
 class Header extends React.Component {
     render() {
         return (
             <header>
+                <div id='user'>
+                    <img src={user} />
+                    <div id='info'>
+                        <p>Miguel José, 9 anos</p>
+                        <p>00 pontos</p>
+                    </div>
+                </div>
                 <ul>
                     <li>Início</li>
                     <li>Banca</li>
