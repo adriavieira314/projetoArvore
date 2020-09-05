24a0700a71097b03d7fba59ea457b7ae2d8ecea5
Adria Vieira <adriavieira314@gmail.com>
2020-09-05 15:01:00 -0400

    estilo do grid-items alterado

--- src/Home.js
+++ src/Home.js
@@ -17,12 +17,12 @@ function App() {
     
       <h2>Nossas Trilhas</h2>
       <section id='container'>
-        <p className='grid-item'>Trilha Botânica</p>
-        <p className='grid-item'>Trilha Monteiro Lobato</p>
-        <p className='grid-item'>Trilha Universo</p>
-        <p className='grid-item'>Trilha Folclore</p>
-        <p className='grid-item'>Trilha Matemática</p>
-        <p className='grid-item'>Trilha Experimentos</p>
+        <p className='grid-item' style={{color: '#008000', borderColor: '#008000'}}>Trilha Botânica</p>
+        <p className='grid-item' style={{color: '#800080', borderColor: '#800080'}}>Trilha Monteiro Lobato</p>
+        <p className='grid-item' style={{color: '#4169E1', borderColor: '#4169E1'}}>Trilha Universo</p>
+        <p className='grid-item' style={{color: '#FFA500', borderColor: '#FFA500'}}>Trilha Folclore</p>
+        <p className='grid-item' style={{color: '#FF0000', borderColor: '#FF0000	'}}>Trilha Matemática</p>
+        <p className='grid-item' style={{color: '#000000', borderColor: '#000000'}}>Trilha Experimentos</p>
       </section>
 
       <article>
