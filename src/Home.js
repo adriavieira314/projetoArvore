d0b3550a8a02eb355cefd7bf5c8eee8b8147f69f
Adria Vieira <adriavieira314@gmail.com>
2020-09-05 11:58:14 -0400

    introduction e headings das seçoes adicionadas

--- src/Home.js
+++ src/Home.js
@@ -6,7 +6,13 @@ function App() {
   return (
     <main>
       <Header />
-
+      <div id='introduction'>
+        <p>
+          Seja bem-vindo Miguel José!<br/> A partir de agora você poderá iniciar sua trilha de conhecimento e compartilhá-la com toda sua família!
+        </p>
+      </div>
+    
+      <h2>Nossas Trilhas</h2>
       <section id='container'>
         <p className='grid-item'>Trilha Botânica</p>
         <p className='grid-item'>Trilha Monteiro Lobato</p>
@@ -15,6 +21,9 @@ function App() {
         <p className='grid-item'>Trilha Matemática</p>
         <p className='grid-item'>Trilha Experimentos</p>
       </section>
+
+      <h2>Ranking Nacional</h2>
+
     </main>
   );
 }
