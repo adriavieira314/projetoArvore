a3a80a9803f42ecb72ffb11c5ffcca3b92844f73
Adria Vieira <adriavieira314@gmail.com>
2020-09-05 21:56:02 -0400

    trilha3 adicionada

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import Home from '../src/TelaInicial';
 import TrilhaInicio from '../src/Trilhas/TrilhaInicial';
 import Trilha1 from './Trilhas/Trilha1/Trilha1';
 import Trilha2 from './Trilhas/Trilha2/Trilha2';
+import Trilha3 from './Trilhas/Trilha3/Trilha3';
 
 const Routes = () => {
     return (
@@ -14,6 +15,7 @@ const Routes = () => {
                 <Route path='/trilha' component={TrilhaInicio} />
                 <Route path='/trilha1' component={Trilha1} />
                 <Route path='/trilha2' component={Trilha2} />
+                <Route path='/trilha3' component={Trilha3} />
             </Switch>
         </BrowserRouter>
     );
