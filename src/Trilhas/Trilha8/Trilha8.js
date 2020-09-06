0494389f0f19d2e76f7bdc05510e40de9bc1d7ad
Adria Vieira <adriavieira314@gmail.com>
2020-09-06 14:47:28 -0400

    imagens adicionadas a trilha 8

--- src/Trilhas/Trilha8/Trilha8.js
+++ src/Trilhas/Trilha8/Trilha8.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Header, UserSide } from '../../Header';
 import { Link } from 'react-router-dom';
+import michelangelo from '../../assets/mestre-michelangelo.jpg';
+import esculturas from '../../assets/esculturas.jpg';
+import './style.css';
 
 class Trilhas extends React.Component {
     
@@ -8,20 +11,18 @@ class Trilhas extends React.Component {
         return (
             <main>
                 <Header />
-                <section id='inicio-trilha'>
+                <section id='inicio'>
                     <UserSide />
-                    <div id='conteudo'>
-                        <div id='box'>
-                            <p>Preciso fazer</p>
-                        </div>
-                        <div style={{justifyContent: 'center'}} id='voltar-avancar'>
-                            <Link to='/trilha9'>
-                                <button>Avançar</button>
-                            </Link>
-                        </div> 
+                    <div id='livros'>
+                        <img src={michelangelo} alt='Livro, O mestre Michelangelo' />
+                        <p>Autor(a): Giselle Vargas<br/>Editora: Alfa e Beto Soluções<br/>Edição: 1ª ed.</p>
+                        <p>Autor(a): On Line Editora<br/>Editora: On Line Editora</p>
+                        <img id='esculturas' src={esculturas} alt='Guia, Esculturas e Monumentos' />
                     </div>
-                    
                 </section>
+                <Link to='/trilha9' id='avancar-livros' className='links'>
+                    <button>Avançar</button>
+                </Link>
             </main>
         );
     }
